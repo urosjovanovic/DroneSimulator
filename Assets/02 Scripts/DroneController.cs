b912a5f8135cd013b05501d254a7c4a18140ed30
Uros <urosh3d@gmail.com>
2015-07-12 10:55:07 +0200

    Test nivo, rig-ovan model drona, DroneController

---
+++ Assets/02 Scripts/DroneController.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class DroneController : MonoBehaviour 
+{
+    private Transform[] rotors;
+    private Vector3 cameraViewDirection;
+
+    //Represents the whole model (root object)
+    public Transform DroneModel;
+
+    //Looking from the top of the drone
+    public Transform FrontLeftRotor;
+    public Transform FrontRightRotor;
+    public Transform BackLeftRotor;
+    public Transform BackRightRotor;
+
+    //Optional params
+    public Camera DroneCamera;
+    public Transform CameraRollSystem;
+    public Transform CameraPitchSystem;
+
+	// Use this for initialization
+	void Start () 
+    {
+        try
+        {
+            if (this.DroneModel == null)
+                throw new ArgumentNullException("No drone model attached");
+
+            if (this.FrontLeftRotor == null || this.FrontRightRotor == null || this.BackLeftRotor == null || this.BackRightRotor == null)
+                throw new ArgumentNullException("One or more rotor objects are missing");
+        }
+        catch
+        {
+            this.enabled = false;
+            throw;
+        }
+
+        this.rotors = new Transform[4]{ this.FrontLeftRotor, this.FrontRightRotor, this.BackLeftRotor, this.BackRightRotor };
+        if (this.HasCamera)
+            this.cameraViewDirection = this.DroneCamera.transform.forward;
+        else
+            this.cameraViewDirection = this.CameraPitchSystem.forward;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        foreach (var rotor in this.rotors)
+            rotor.RotateAround(rotor.GetObjectCenter(), this.DroneModel.up, 3600*Time.deltaTime);
+
+        //Simulating camera gyro
+        if (this.CameraRollSupported)
+            this.CameraRollSystem.up = Vector3.up;
+        if (this.CameraPitchSupported)
+            this.CameraPitchSystem.forward = this.cameraViewDirection;
+	}
+
+    private bool HasCamera
+    {
+        get { return this.DroneCamera != null; }
+    }
+
+    private bool CameraRollSupported
+    {
+        get { return this.CameraRollSystem != null; }
+    }
+
+    private bool CameraPitchSupported
+    {
+        get { return this.CameraPitchSystem != null; }
+    }
+}
---
+++ Assets/02 Scripts/ExtensionMethods.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class ExtensionMethods
+{
+    public static Vector3 GetObjectCenter(this Transform transform)
+    {
+        Renderer[] renderers = transform.GetComponentsInChildren<Renderer>();
+        if (renderers.Length > 0)
+        {
+            Vector3 centroid = Vector3.zero;
+            foreach (var renderer in renderers)
+                centroid += renderer.bounds.center;
+            centroid /= renderers.Length;
+            return centroid;
+        }
+        else
+            return transform.transform.position;
+    }
+}
