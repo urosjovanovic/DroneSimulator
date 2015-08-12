a1cdebb32482b36ff9a77de8d8d2814d22b94d22
TheDivic <divicnikola@gmail.com>
2015-08-12 16:33:35 +0200

    Up/down RTS camera rotation, camera tweaking

--- Assets/RTSCameraController.cs
+++ Assets/RTSCameraController.cs
@@ -3,10 +3,13 @@ using System.Collections;
 
 public class RTSCameraController : MonoBehaviour {
 	public float scrollSpeed = 1;
-	public float rotateSpeed = 50;
+	public float LrRotateSpeed = 50;
+	public float UdRotateSpeed = 30;
+	public float zoomSpeed = 20;
 
 	float mouseX = 0;
 	float mouseY = 0;
+	
 	// Use this for initialization
 	void Start () {
 	
@@ -47,13 +50,28 @@ public class RTSCameraController : MonoBehaviour {
 			var cursorCoords = gameObject.GetComponent<Camera>().ScreenToWorldPoint(rotateAround);
 
 			if (mouseX < 0) {
-				gameObject.transform.RotateAround(cursorCoords, Vector3.up, rotateSpeed * Time.deltaTime);
+				gameObject.transform.RotateAround(cursorCoords, Vector3.up, LrRotateSpeed * Time.deltaTime);
 			} 
 			else if(mouseX > Screen.width){
-				gameObject.transform.RotateAround(cursorCoords, Vector3.up, -rotateSpeed * Time.deltaTime);
+				gameObject.transform.RotateAround(cursorCoords, Vector3.up, -LrRotateSpeed * Time.deltaTime);
 			}
 
+			if (mouseY < 0) { 
+				gameObject.transform.Rotate(Vector3.right, UdRotateSpeed * Time.deltaTime);
+			} 
+			
+			if (mouseY > Screen.height) { 
+				gameObject.transform.Rotate(Vector3.right, -UdRotateSpeed * Time.deltaTime);
+			} 
+
 			//gameObject.transform.RotateAround(cursorCoords, Vector3.up, 
+		}
+
+		float scrollWheel = Input.GetAxis("Mouse ScrollWheel");
+		if (scrollWheel > 0 && gameObject.transform.position.y > 1) {
+			gameObject.transform.Translate (Vector3.forward * scrollWheel * zoomSpeed * Time.deltaTime);
+		} else if (scrollWheel < 0 && gameObject.transform.position.y < 20) {
+			gameObject.transform.Translate (Vector3.forward * scrollWheel * zoomSpeed * Time.deltaTime);
 		}
 	}
 }
