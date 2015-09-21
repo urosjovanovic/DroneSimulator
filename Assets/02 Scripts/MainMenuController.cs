363e21790c7d8e59646d1fd029ee972d6b25d7a4
Uros <urosh3d@gmail.com>
2015-09-21 08:09:53 +0200

    Redizajniran MainMenu

--- Assets/02 Scripts/MainMenuController.cs
+++ Assets/02 Scripts/MainMenuController.cs
@@ -2,6 +2,19 @@
 
 public class MainMenuController : MonoBehaviour
 {
+    private GameObject drone;
+
+    private void Start()
+    {
+        this.drone = GameObject.Find("DJIPhantom");
+    }
+
+    private void Update()
+    {
+        if(this.drone != null)
+            this.drone.transform.Rotate(Vector3.up, 10.0f * Time.deltaTime, Space.World);
+    }
+
     public void StartArcadeMode()
     {
         Application.LoadLevel("MazeRunnerScene");
